a88b43c19331365b0784b0698bfeca66459fa05c
Darren Bathgate <darren@Darren-Bathgates-MacBook-Pro.local>
2011-02-09 13:42:45 -0500

    LDAP unit test complete

---
+++ src/test/java/knzn/ldap/ContextFactory.java
@@ -0,0 +1,21 @@
+package knzn.ldap;
+
+import java.util.Hashtable;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.naming.directory.InitialDirContext;
+
+public class ContextFactory {
+	private static Hashtable<String, String> env = 
+		InitialDirectoryContextFactory.createBaseEnv(
+				"uid=admin, ou=system", "secret", "o=sevenSeas", "simple");
+	
+	public static InitialContext create() throws NamingException{
+        return new InitialContext(env);
+	}
+	
+	public static InitialDirContext createDir() throws NamingException{
+		return new InitialDirContext(env);
+	}
+}
--- src/test/java/knzn/ldap/LdapTest.java
+++ src/test/java/knzn/ldap/LdapTest.java
@@ -1,12 +1,15 @@
 package knzn.ldap;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Set;
 
 import javax.naming.InitialContext;
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
@@ -14,6 +17,7 @@ import javax.naming.directory.BasicAttribute;
 import javax.naming.directory.BasicAttributes;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
 
 import org.apache.directory.server.unit.AbstractServerTest;
 import org.apache.directory.server.core.configuration.MutablePartitionConfiguration;
@@ -23,59 +27,26 @@ public class LdapTest extends AbstractServerTest {
 	@Override
 	protected void setUp() throws Exception {
 		
-		MutablePartitionConfiguration mpc = new MutablePartitionConfiguration();
-		mpc.setName("Test");
-		mpc.setSuffix("o=test");
-		
-		Set<String> index = new HashSet<String>();
-		index.add("objectClass");
-		index.add("o");
-		index.add("cn");
-		index.add("uid");
-		
-		mpc.setIndexedAttributes(index);
-		
-		Attributes atts = new BasicAttributes(true);
-		
-		Attribute att = new BasicAttribute("objectClass");
-		att.add("top");
-		att.add("organization");
-		atts.put(att);
-		
-		att = new BasicAttribute("o");
-		att.add("test");
-		atts.put(att);
-		
-		att = new BasicAttribute("cn");
-		att.add("color");
-		att.add("people");
-		att.add("shape");
-		atts.put(att);
-		
-		att = new BasicAttribute("uid");
-		att.add("dbathgate");
-		atts.put(att);
-		
-		mpc.setContextEntry(atts);
-		
-		Set<MutablePartitionConfiguration> smpc = new HashSet<MutablePartitionConfiguration>();
-		smpc.add(mpc);
-		
-		configuration.setContextPartitionConfigurations(smpc);
-		
-		File file = new File("/target/ldap/server-work");
-		
-		configuration.setWorkingDirectory(file);
+		setupPartition();
 		
 		super.setUp();
+		
+		InputStream io = new FileInputStream(new File("src/test/resources/demo.ldif"));
+		importLdif(io);
 	}
+	
+    public void testImport() throws NamingException
+    {
+        Set<String> result = searchDNs( "(ObjectClass=*)", "", 
+                                          SearchControls.ONELEVEL_SCOPE );
+
+        assertTrue( result.contains( "ou=groups,o=sevenSeas" ) );
+        assertTrue( result.contains( "ou=people,o=sevenSeas" ) );
+    }
 	
 	public void testPartition() throws NamingException{		
 		
-		Hashtable<String, String> env = 
-			InitialDirectoryContextFactory.createBaseEnv("uid=admin, ou=system", "secret", "o=Test", "simple");
-		
-        InitialContext ic = new InitialContext(env);
+		InitialContext ic = ContextFactory.create();
 
         DirContext dir = (DirContext) ic.lookup("");
         
@@ -84,7 +55,7 @@ public class LdapTest extends AbstractServerTest {
         Attributes attributes = dir.getAttributes("");
         
         assertNotNull(attributes);
-        assertEquals("test", attributes.get("o").get());
+        assertEquals("sevenseas", attributes.get("o").get());
 
         Attribute attribute = attributes.get("objectClass");
         
@@ -95,8 +66,8 @@ public class LdapTest extends AbstractServerTest {
 	}
 	
 	public void testSearch(){
-		Hashtable<String, String> env = 
-			InitialDirectoryContextFactory.createBaseEnv("uid=admin, ou=system", "secret", "o=Test", "simple");
+		Hashtable<String, String> env =
+			InitialDirectoryContextFactory.createBaseEnv("uid=admin, ou=system", "secret", "o=sevenSeas", "simple");
 		
 		LdapTemplate temp = new LdapTemplate(env);
 		
@@ -108,13 +79,67 @@ public class LdapTest extends AbstractServerTest {
 			}
 		};
 		SearchControls search = new SearchControls();
-		search.setSearchScope(SearchControls.OBJECT_SCOPE);
+		search.setSearchScope(SearchControls.ONELEVEL_SCOPE);
 		
-		List<Object> list =  temp.search("", "(uid=*)", result, search);
+		List<Object> list =  temp.search("", "(objectClass=*)", result, search);
 	
 		assertNotNull(list);
 	}
 	
+    private Set<String> searchDNs( String filter, String base, int scope ) 
+    throws NamingException
+ {
+     DirContext appRoot = ContextFactory.createDir();
+
+     SearchControls controls = new SearchControls();
+     controls.setSearchScope( scope );
+     NamingEnumeration result = appRoot.search( base, filter, controls );
+
+     // collect all results
+     HashSet<String> entries = new HashSet<String>();
+
+     while ( result.hasMore() )
+     {
+         SearchResult entry = ( SearchResult ) result.next();
+         entries.add( entry.getName() );
+     }
+
+     return entries;
+ }
+	
+	private void setupPartition() throws NamingException{
+
+        MutablePartitionConfiguration pcfg = new MutablePartitionConfiguration();
+        pcfg.setName( "sevenSeas" );
+        pcfg.setSuffix( "o=sevenseas" );
+
+        Set<String> indexedAttrs = new HashSet<String>();
+        indexedAttrs.add( "objectClass" );
+        indexedAttrs.add( "o" );
+        pcfg.setIndexedAttributes( indexedAttrs );
+
+        Attributes attrs = new BasicAttributes( true );
+
+        Attribute attr = new BasicAttribute( "objectClass" );
+        attr.add( "top" );
+        attr.add( "organization" );
+        attrs.put( attr );
+
+        attr = new BasicAttribute( "o" );
+        attr.add( "sevenseas" );
+        attrs.put( attr );
+
+        pcfg.setContextEntry( attrs );
+
+        Set<MutablePartitionConfiguration> pcfgs = new HashSet<MutablePartitionConfiguration>();
+        pcfgs.add( pcfg );
+
+        configuration.setContextPartitionConfigurations( pcfgs );
+
+        File workingDirectory = new File( "server-work" );
+        configuration.setWorkingDirectory( workingDirectory );
+
+	}
 	
 	@Override
 	protected void tearDown() throws Exception {
